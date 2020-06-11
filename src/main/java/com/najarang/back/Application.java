fcfdfd5f46ba0036fb6bbbda70825d55703724a8
jalhagosipo <gpfla757@naver.com>
2020-06-11 23:12:03 +0900

    chore: apply war

--- src/main/java/com/najarang/back/Application.java
+++ src/main/java/com/najarang/back/Application.java
@@ -2,12 +2,19 @@ package com.najarang.back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+    // war로 패키징하기위해 추가한 메소드
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
 }
