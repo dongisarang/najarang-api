d8edf1b97b4de434c045e16fd5c6ad4c41161a50
jalhagosipo <gpfla757@naver.com>
2020-06-12 21:16:22 +0900

    chore: apply jar

--- src/main/java/com/najarang/back/Application.java
+++ src/main/java/com/najarang/back/Application.java
@@ -8,12 +8,6 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
-    // war로 패키징하기위해 추가한 메소드
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
