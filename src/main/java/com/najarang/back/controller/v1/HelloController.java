17b2c0b092b181afbfa93be552d4d674d2abd8e0
jalhagosipo <gpfla757@naver.com>
2020-06-21 00:24:25 +0900

    feat: add swagger library

--- src/main/java/com/najarang/back/controller/v1/HelloController.java
+++ src/main/java/com/najarang/back/controller/v1/HelloController.java
@@ -1,11 +1,15 @@
 package com.najarang.back.controller.v1;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
+@Api(tags = {"0. TestHello"})
 @RestController
 @RequestMapping("/v1")
 public class HelloController {
 
+    @ApiOperation(value = "테스트 컨트롤러", notes = "HelloWorld를 보여준다")
     @GetMapping("/hello")
     public String hello() {
         return "HelloWorld";
