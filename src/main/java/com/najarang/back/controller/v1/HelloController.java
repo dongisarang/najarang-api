5bfbe255490a39c574010d53c160696cf2dbbb99
jalhagosipo <gpfla757@naver.com>
2020-07-18 19:51:53 +0900

    feat: use h2

--- src/main/java/com/najarang/back/controller/v1/HelloController.java
+++ src/main/java/com/najarang/back/controller/v1/HelloController.java
@@ -2,7 +2,9 @@ package com.najarang.back.controller.v1;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Api(tags = {"0. TestHello"})
 @RestController
---
+++ src/main/java/com/najarang/back/repo/UserJpaRepo2.java
@@ -0,0 +1,6 @@
+package com.najarang.back.repo;
+
+import com.najarang.back.entity.User2;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserJpaRepo2 extends JpaRepository<User2, Long> {}
