657e4bea0fdf8d6ae83649b017faba30b178ad37
jalhagosipo <gpfla757@naver.com>
2020-07-19 21:06:42 +0900

    fix: change to use requestdispatcher

--- src/main/java/com/najarang/back/config/security/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/najarang/back/config/security/CustomAuthenticationEntryPoint.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -21,6 +22,7 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException ex) throws IOException,
             ServletException {
-        response.sendRedirect("/exception/entrypoint");
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/exception/entrypoint");
+        dispatcher.forward(request, response);
     }
 }
