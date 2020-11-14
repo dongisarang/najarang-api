60b348b81215c8e5c9063d3039455fce7a5bae2a
jalhagosipo <gpfla757@naver.com>
2020-11-15 02:02:30 +0900

    chore: use helloworld to test

--- src/main/java/com/najarang/back/security/WebConfig.java
+++ src/main/java/com/najarang/back/security/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     private static final String[] EXCLUDE_PATHS = {
             "/signin",
             "/signup",
-            "/users",
+            "/helloworld/**",
             "/error/**"
     };
 
