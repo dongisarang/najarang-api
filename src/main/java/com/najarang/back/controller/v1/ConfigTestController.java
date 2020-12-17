2cb7993f4f7510ee6148755e7f05a18296329eb5
jalhagosipo <gpfla757@naver.com>
2020-12-17 23:51:02 +0900

    feat: use spring cloud config server

---
+++ src/main/java/com/najarang/back/controller/v1/ConfigTestController.java
@@ -0,0 +1,19 @@
+package com.najarang.back.controller.v1;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+@RestController
+@RefreshScope
+public class ConfigTestController {
+
+    @Value("${spring.profiles}")
+    private String identity;
+
+    @GetMapping("/test")
+    public String test() {
+        return identity;
+    }
+}
