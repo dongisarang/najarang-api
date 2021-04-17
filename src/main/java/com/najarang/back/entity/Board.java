b732820d9dda3a29d8300a1735cb1f63fc9c0bb8
jalhagosipo <gpfla757@naver.com>
2021-04-17 14:05:37 +0900

    refactor: default hitCount/likeCount 0

--- src/main/java/com/najarang/back/entity/Board.java
+++ src/main/java/com/najarang/back/entity/Board.java
@@ -2,6 +2,8 @@ package com.najarang.back.entity;
 
 import com.najarang.back.dto.BoardDTO;
 import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.*;
@@ -10,11 +12,12 @@ import javax.persistence.*;
 @AllArgsConstructor
 @Entity
 @DynamicUpdate
+@DynamicInsert
 @Getter
 @ToString
 @Builder
 @Table(name = "board")
-public class Board extends BaseTime{
+public class Board extends BaseTime {
 
     @Id // primaryKey임
     @GeneratedValue(strategy = GenerationType.IDENTITY) //  pk 필드를 auto_increment로 설정
@@ -30,10 +33,17 @@ public class Board extends BaseTime{
     @ManyToOne
     @JoinColumn(name = "TOPIC_ID")
     private Topic topic;
+    // @ColumnDefault("0") ddl로 default 설정
     @Column(name = "LIKE_COUNT")
     private Long likeCount;
     @Column(name = "HIT_COUNT")
     private Long hitCount;
+
+    @PrePersist
+    public void prePersist() {
+        this.hitCount = this.hitCount == null ? 0 : this.hitCount;
+        this.likeCount = this.likeCount == null ? 0 : this.likeCount;
+    }
 
     public BoardDTO toDTO(){
         Board board = new Board(this.id, this.title, this.content, this.user, this.topic, this.likeCount, this.hitCount);
