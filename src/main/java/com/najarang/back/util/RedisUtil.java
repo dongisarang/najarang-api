f79ce72dd88340c2bf5bc6fb56809df0244e265f
jalhagosipo <gpfla757@naver.com>
2021-07-28 21:12:19 +0900

    chore: comment redis

--- src/main/java/com/najarang/back/util/RedisUtil.java
+++ src/main/java/com/najarang/back/util/RedisUtil.java
@@ -7,6 +7,13 @@ import org.springframework.stereotype.Service;
 
 import java.time.Duration;
 
+/*
+* Redis는 RedisTemplate을 통해서 Redis 서버와 통신함
+* - RedisTemplate : Redis module 을 스프링에서 제공함을 통해서 사용자가 좀 더 쉽게 쓸수 있도록 다양한 기능들을 제공
+* 높은 수준의 추상화를 통해서, 오퍼레이션들을 제공
+*
+* ValueOperations : Redis string (or value) operations
+* */
 @Service
 @RequiredArgsConstructor
 public class RedisUtil {
@@ -24,6 +31,7 @@ public class RedisUtil {
     }
 
     public void setDataExpire(String key,String value,long duration){
+        // expireDuration기간 동안 저장
         ValueOperations<String,String> valueOperations = stringRedisTemplate.opsForValue();
         Duration expireDuration = Duration.ofSeconds(duration);
         valueOperations.set(key,value,expireDuration);
