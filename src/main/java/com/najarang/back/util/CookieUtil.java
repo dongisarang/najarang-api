ddbb1d5591f7a5a6bb3a3480f7f0c600d4ddca12
jalhagosipo <gpfla757@naver.com>
2021-08-03 02:28:15 +0900

    chore: add cookie domain

--- src/main/java/com/najarang/back/util/CookieUtil.java
+++ src/main/java/com/najarang/back/util/CookieUtil.java
@@ -21,6 +21,7 @@ public class CookieUtil {
         token.setHttpOnly(true); // 쿠키는 클라이언트 측 스크립트에 접근x. 악성 스크립트(XSS 공격)에 의한 위험 완화
         token.setMaxAge(age); // 쿠키 유효기간 설정
         token.setPath("/"); // 모든 경로에서 접근 가능하도록 설정
+        token.setDomain("najarang.com");
         return token;
     }
 
